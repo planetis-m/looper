23a04a4731d082a8ae490c727186f42509690517
Antonis <b3liever@yandex.com>
2019-12-30 22:33:58 +0200

    Fix for collect

--- looper.nim
+++ looper.nim
@@ -7,7 +7,7 @@ proc trans(n, res, bracketExpr: NimNode): (NimNode, NimNode, NimNode) =
       result[0] = copyNimTree(n)
       result[1] = copyNimTree(n)
       result[2] = copyNimTree(n)
-      for i in ord(n.kind == nnkCaseStmt)..<n.len:
+      for i in ord(n.kind == nnkCaseStmt) ..< n.len:
          (result[0][i], result[1][^1], result[2][^1]) = trans(n[i], res, bracketExpr)
    of nnkStmtList, nnkStmtListExpr, nnkBlockStmt, nnkBlockExpr, nnkWhileStmt,
          nnkForStmt, nnkElifBranch, nnkExceptBranch, nnkOfBranch, nnkElse, nnkElifExpr:
@@ -21,8 +21,8 @@ proc trans(n, res, bracketExpr: NimNode): (NimNode, NimNode, NimNode) =
       result[1] = n[0][0]
       result[2] = n[0][1]
       if bracketExpr.len == 0:
-         bracketExpr.add(bindSym"initTable", newCall(bindSym"typeof", newEmptyNode()),
-            newCall(bindSym"typeof", newEmptyNode()))
+         bracketExpr.add(bindSym"initTable", newCall(bindSym"typeof",
+               newEmptyNode()), newCall(bindSym"typeof", newEmptyNode()))
       template adder(res, k, v) = res[k] = v
       result[0] = getAst(adder(res, n[0][0], n[0][1]))
    of nnkCurly:
@@ -120,15 +120,15 @@ macro enumerate*(x: ForLoopStmt): untyped =
    var newFor = newTree(nnkForStmt)
    if x.len == 3: # single iteration variable
       if x[0].kind == nnkVarTuple: # for (x, y, ...) in iter
-         result.add newVarStmt(x[0][0], newLit(0))
-         body.add newCall(bindSym"inc", x[0][0])
+         result.add newVarStmt(x[0][0], newLit(-1))
+         body.insert(0, newCall(bindSym"inc", x[0][0]))
          for i in 1 .. x[0].len-2:
             newFor.add x[0][i]
-      else:
+      else: #TODO support them
          error("enumerate iterator doesn't support tuples") # for x in iter
    else: # for x, y, ... in iter
-      result.add newVarStmt(x[0], newLit(0))
-      body.add newCall(bindSym"inc", x[0])
+      result.add newVarStmt(x[0], newLit(-1))
+      body.insert(0, newCall(bindSym"inc", x[0]))
       for i in 1 .. x.len-3:
          newFor.add x[i]
    # transform enumerate(X) to 'X'
@@ -145,34 +145,40 @@ when isMainModule:
       c = @["one", "two", "three", "four", "five"]
 
    block:
-      var res: seq[(int,)]
+      var res: seq[(int, )]
       for x in zip(a):
          res.add x
-      assert res == @[(1,), (3,), (5,), (7,)]
-
+      assert res == @[(1, ), (3, ), (5, ), (7, )]
    block:
       var res: seq[(int, int, int)]
       for (x, y, z) in zip(a, b, @[-1, -2, -3, -4, -5]):
          res.add (x, y, z)
       assert res == @[(1, 0, -1), (3, 2, -2), (5, 4, -3), (7, 6, -4)]
-
    block:
       var res: seq[(int, int)]
       for x in zip(b, a):
          res.add x
       assert res == @[(0, 1), (2, 3), (4, 5), (6, 7)]
-
    block:
       var res: seq[(int, int)]
       for x, y in zip(a, b):
          res.add (y, x)
       assert res == @[(0, 1), (2, 3), (4, 5), (6, 7)]
-
    block:
       var res: seq[(int, int, string)]
       for (x, y, z) in zip(a, b, c):
          res.add (x, y, z)
       assert res == @[(1, 0, "one"), (3, 2, "two"), (5, 4, "three"), (7, 6, "four")]
+   block:
+      var res: seq[(int, int)]
+      for i, x in enumerate(a):
+         res.add (i, x)
+      assert res == @[(0, 1), (1, 3), (2, 5), (3, 7)]
+   block:
+      var res: seq[(int, int)]
+      for (i, x) in enumerate(a.items):
+         res.add (i, x)
+      assert res == @[(0, 1), (1, 3), (2, 5), (3, 7)]
 
    var data = @["bird", "word"]
    assert collect(for (i, d) in data.pairs: (if i mod 2 == 0: d)) == @["bird"]
@@ -182,8 +188,12 @@ when isMainModule:
    import strutils
    assert collect(for d in data.items: (try: parseInt(d) except: 0)) == @[0, 0]
    let x = collect:
-      for (i, d) in data.pairs:
+      for d in data.items:
          case d
          of "bird": "word"
          else: d
    assert x == @["word", "word"]
+   assert collect(for (i, d) in enumerate(data.items): (i, d)) == @[(0, "bird"),
+         (1, "word")]
+   assert collect(for (i, d) in enumerate(data.items): {i: d}) == {1: "word",
+         0: "bird"}.toTable
